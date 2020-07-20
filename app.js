d1659ffed0623bfd88b4e5c86e43b12664b8cf41
Javon Whye <55068535+JavonWhyte@users.noreply.github.com>
2020-07-20 18:42:18 -0500

    Update app.js
    Root Get Handler modified

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 9000
 
 app.get('/', (req, res) => {
-    res.send(`<h2>Server has started on ${PORT}</h2>`)
+    res.send(`<h2>Chef's Mate Server has started on ${PORT}</h2>`)
 })
 
 app.listen(PORT, () => {
