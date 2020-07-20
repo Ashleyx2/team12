0672988c08759a2219c0fb3c9261fab532ee56a9
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-07-20 15:16:25 -0500

    Added root get handler

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 9000
 
+app.get('/', (req, res) => {
+    res.send(`<h2>Server has started on ${PORT}</h2>`)
+})
+
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`)
 })
